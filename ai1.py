2b682e01521101b1f819988ad48d63524aa237ba
charmi582 <h927889932@gmail.com>
2025-05-08 19:02:08 +0800

    ai chat

--- ai1.py
+++ ai1.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -7,12 +7,12 @@ y=[1, 1, 1, 1, 1, 0, 0, 0, 0]
 
 x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.3)
 
-model = LinearRegression()
+model = LogisticRegression()
 model.fit(x_train, y_train)
 
 y_pred=model.predict(x_test)
 acc=accuracy_score(y_test, y_pred)
 print("準確率:" ,acc)
 
-print("如果我上一餐吃了2 這餐吃得下嗎", model.predict([[2]]))
+print("睡 6.8 小時會精神嗎？預測為：", model.predict([[6.8]]))
 
